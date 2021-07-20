7f7b4eeb0f5cfc9b83ed80ba5cc18267eab94065
junghoon <jh981113@naver.com>
2021-07-21 00:32:37 +0900

    Read CommitMessage.md; crawling.py modified;

--- data/crawling.py
+++ data/crawling.py
@@ -7,51 +7,58 @@ from selenium.webdriver import Chrome, ChromeOptions
 import time
 import os
 import warnings
+import glob
+
 warnings.filterwarnings(action='ignore')
 
-get_ipython().run_line_magic('load_ext', 'watermark')
-get_ipython().run_line_magic('watermark', '-asoyeong -d -n -v -ppytube,bs4,selenium')
+
+# get_ipython().run_line_magic('load_ext', 'watermark')
+# get_ipython().run_line_magic('watermark', '-asoyeong -d -n -v -ppytube,bs4,selenium')
 
 # get_driver - 드라이버 가져오기
 def get_driver():
     options = ChromeOptions()
     options.add_argument('headless')
     options.add_argument('disable-gpu')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+    # options.add_argument(
+    #     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     driver = Chrome(options=options)
     return driver
+
 
 # search - 검색 결과 page source 리턴
 def search(keyword):
     driver = get_driver()
-    
+
     url = 'https://www.youtube.com/results?search_query=' + keyword
     driver.get(url)
-    
-    last_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-    
-    for i in range(0, 100): # 스크롤 100번까지로 제한
-        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-        
-        time.sleep(3)
-        
-        
-        new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-        
-        if new_page_height == last_page_height:
-            break
-            
-        last_page_height = new_page_height
-        
+
+    # last_page_height = driver.execute_script("return document.documentElement.scrollHeight")
+    #
+    # pages = 10
+    # for i in range(pages):  # 스크롤 100번까지로 제한
+    #     driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+    #
+    #     # time.sleep(3)
+    #
+    #     new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
+    #
+    #     if new_page_height*i > last_page_height:
+    #         break
+    #
+    #     last_page_height = new_page_height
+
     dom = BeautifulSoup(driver.page_source, 'lxml')
     return dom
 
+
 # get_url - 검색 결과창의 모든 동영상 url 받아오기
-def get_url():
+def get_url(keyword):
     dom = search(keyword)
     href = [a.attrs['href'] for a in dom.select('a#video-title')]
     url = ['https://www.youtube.com' + h for h in href]
     return url
+
 
 # chk_cap - 한국어 자막 유무 체크 (없으면 None 리턴)
 def chk_cap(yt):
@@ -59,11 +66,12 @@ def chk_cap(yt):
     yt_cap = yt.captions.get_by_language_code(lang)
     return yt_cap
 
+
 # get_audio - pytube 라이브러리를 사용하여 오디오 파일 받아오기
-def get_audio():
-    path = 'C:/Users/USER/Desktop/STUDY/데이터 청년 캠퍼스/project/audio'
-    url = get_url()
-    url = list(set(url)) # 중복 제거
+def get_audio(keyword):
+    path = './audio_data/'
+    url = get_url(keyword)
+    url = list(set(url))  # 중복 제거
     for _ in url:
         try:
             yt = YouTube(_)
@@ -78,15 +86,16 @@ def get_audio():
         except Exception:
             continue
 
+
 # get_cap - pytube 라이브러리를 사용하여 자막 파일 받아오기
-def get_cap():
-    path = 'C:/Users/USER/Desktop/STUDY/데이터 청년 캠퍼스/project/caption'
-    url = get_url()
-    url = list(set(url)) # 중복 제거
+def get_cap(keyword):
+    path = './caption/'
+    url = get_url(keyword)
+    url = list(set(url))  # 중복 제거
     for _ in url:
         try:
             yt = YouTube(_)
-            if chk_cap(yt) != None and yt.length < 1200: # 한글 자막이 있고 20분 이내인 동영상일 경우
+            if chk_cap(yt) is not None and yt.length < 1200:  # 한글 자막이 있고 20분 이내인 동영상일 경우
                 print(yt.captions.all())
                 out_file = yt.captions['ko'].download(title=yt.title, output_path=path)
                 base, ext = os.path.splitext(out_file)
@@ -96,3 +105,15 @@ def get_cap():
                 print(yt.title + " 다운로드 완료")
         except Exception:
             continue
+
+
+if __name__ == "__main__":
+    f = open("search_keywords.txt", "r") # reading keyword list file
+    lines = f.readlines()
+    for keyword in lines:
+        keyword = keyword.strip()
+        print("Searching youtube data about " + keyword)
+        get_audio(keyword)
+        get_cap(keyword)
+
+    f.close()
