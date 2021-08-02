a3ce5b7529279c860f88219c175d5b744445fe16
soyeongsohn <60024018+soyeongsohn@users.noreply.github.com>
2021-08-02 12:33:48 +0900

    Add files via upload

---
+++ romanizer/romanizer.py
@@ -0,0 +1,16 @@
+!pip install korean_romanizer
+!pip install g2pk
+!pip install pororo
+
+from korean_romanizer.romanizer import Romanizer
+from pororo import Pororo
+
+class KoreanRomanizer:
+  def __init__(self):
+    self.sent = ""
+    print("Romanizer Initialized")
+  def romanize(self, sent):
+    self.sent = sent
+    g2p = Pororo(task="g2p", lang="ko")
+    r = Romanizer(g2p(self.sent))
+    return r.romanize()
