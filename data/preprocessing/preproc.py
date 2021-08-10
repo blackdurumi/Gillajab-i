1be39bb5cdd817ca3f34ec2970c2a8ecb62fd701
junghoon <jh981113@naver.com>
2021-08-10 15:56:50 +0900

    add docstring(preproc.py)

--- data/preprocessing/preproc.py
+++ data/preprocessing/preproc.py
@@ -3,10 +3,23 @@ import shutil
 from aihub_txt_preprocessing import sentence_filter
 
 if __name__ == "__main__":
+    """
+    aihub_data_dir 에 있는 aihub 데이터를
+    현재 폴더(preprocessing)내에 processed 폴더에
+    kaldi style로 변환하여 저장해주는 py파일
+    
+    실행 전 aihub_data_dir를 잘 지정하자
+    """
+    aihub_data_dir = '../[원천]8.쇼핑_shopping_01/data/remote/KoreanSpeech/data/2_Validation/raw/09_쇼핑/shopping_01'
     proc_dir = os.getcwd() + "\\processed"
-    trans = open("./processed/trans.txt", "w")
-    os.chdir('../[원천]8.쇼핑_shopping_01/data/remote/KoreanSpeech/data/2_Validation/raw/09_쇼핑/shopping_01')
 
+    # kaldi style을 보면 trans.txt에 wav파일의 id와 text가 저장되어있음. 이를 생성하기 위함
+    trans = open("./processed/shopping.trans.txt", "w")
+
+    # json, wav, txt파일이 있는 폴더의 부모 폴더로 위치 변경 해줌
+    os.chdir(aihub_data_dir)
+
+    # 이후 하위 폴더를 순회하면서 kaldi 디렉토리 style로 변환해줌
     for dlist in os.listdir():
         os.chdir(dlist)
         flist = os.listdir()
