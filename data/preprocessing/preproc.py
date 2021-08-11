36cda6587836d0aab92317711bb837f4053bc31c
junghoon <jh981113@naver.com>
2021-08-11 16:14:31 +0900

    update preproc.py : exploring aihub_data_dir recursively and pre-processing

--- data/preprocessing/preproc.py
+++ data/preprocessing/preproc.py
@@ -2,36 +2,42 @@ import os
 import shutil
 from aihub_txt_preprocessing import sentence_filter
 
+
+def search(dirname):
+    try:
+        flist = os.listdir(dirname)
+        for file in flist:
+            full_filename = dirname+'/'+file
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                if file[-3:] == "wav":
+                    shutil.copy(full_filename, proc_dir)
+                elif file[-3:] == "txt":
+                    curfile = open(full_filename, "r", encoding='utf-8').read()
+                    if curfile is None:
+                        continue
+                    curfile = sentence_filter(curfile)
+                    trans.write(file[:-4] + " " + curfile + "\n")
+                elif file[-4:] == "json":
+                    pass
+    except PermissionError:
+        pass
+
+
 if __name__ == "__main__":
     """
-    aihub_data_dir 에 있는 aihub 데이터를
-    현재 폴더(preprocessing)내에 processed 폴더에
+    aihub_data_dir 에 있는 aihub 데이터(압축 해제된 상태이어야 함)를
+    processed 폴더에
     kaldi style로 변환하여 저장해주는 py파일
-    
-    실행 전 aihub_data_dir를 잘 지정하자
     """
-    aihub_data_dir = '../[원천]8.쇼핑_shopping_01/data/remote/KoreanSpeech/data/2_Validation/raw/09_쇼핑/shopping_01'
-    proc_dir = os.getcwd() + "\\processed"
+    aihub_data_dir = "../aihub_data"
+    proc_dir = os.path.join(os.getcwd(), "processed")
 
     # kaldi style을 보면 trans.txt에 wav파일의 id와 text가 저장되어있음. 이를 생성하기 위함
-    trans = open("./processed/shopping.trans.txt", "w")
+    trans = open("./processed/trans.txt", "w")
 
-    # json, wav, txt파일이 있는 폴더의 부모 폴더로 위치 변경 해줌
-    os.chdir(aihub_data_dir)
-
-    # 이후 하위 폴더를 순회하면서 kaldi 디렉토리 style로 변환해줌
-    for dlist in os.listdir():
-        os.chdir(dlist)
-        flist = os.listdir()
-        for file in flist:
-            if file[-3:] == "wav":
-                shutil.copy(os.getcwd() + '/' + file, proc_dir)
-            elif file[-3:] == "txt":
-                curfile = open(file, "r", encoding='utf-8').read()
-                curfile = sentence_filter(curfile)
-                trans.write(file[:-4] + " " + curfile + "\n")
-            elif file[-4:] == "json":
-                pass
-        os.chdir('..')
+    # 이후 aihub_data_dir 하위 폴더를 순회하면서 kaldi 디렉토리 style로 변환해줌
+    search(aihub_data_dir)
 
     trans.close()
